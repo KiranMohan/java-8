087eb17fb3c908bc0e380d56cd246d9fbb424c0c
Kiran Mohan <kiranmohan@123.com>
2015-03-02 09:56:49 +0530

    add module: excercises from Java 8 for the Really Impatient

---
+++ java8-for-the-really-impatient/chapter-3/src/main/java/ImageDemo.java
@@ -0,0 +1,61 @@
+/*
+ * 12. Enhance the LatentImage class in Section 3.6, “Laziness,” on page 56, so 
+ * that it supports both UnaryOperator<Color> and ColorTransformer. Hint: Adapt 
+ * the former to the latter.
+ */
+import java.util.*;
+import java.util.function.*;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.stage.*;
+
+@FunctionalInterface
+interface ColorTransformer {
+   Color apply(int x, int y, Color colorAtXY);
+}
+
+class LatentImage {
+   private Image in;
+   private List<UnaryOperator<Color>> pendingOperations;
+
+   public static LatentImage from(Image in) {
+      LatentImage result = new LatentImage();
+      result.in = in;
+      result.pendingOperations = new ArrayList<>();
+      return result;
+   }
+
+   LatentImage transform(UnaryOperator<Color> f) {
+      pendingOperations.add(f);
+      return this;
+   }
+
+   public Image toImage() {
+      int width = (int) in.getWidth();
+      int height = (int) in.getHeight();
+      WritableImage out = new WritableImage(width, height);
+      for (int x = 0; x < width; x++)
+         for (int y = 0; y < height; y++) {
+            Color c = in.getPixelReader().getColor(x, y);
+            for (UnaryOperator<Color> f : pendingOperations) c = f.apply(c);
+            out.getPixelWriter().setColor(x, y, c);
+         }
+      return out;
+   }
+}
+
+public class ImageDemo extends Application {
+   public void start(Stage stage) {
+      Image image = new Image("eiffel-tower.jpg");
+      Image finalImage = LatentImage.from(image)
+         .transform(Color::brighter).transform(Color::grayscale)
+         .toImage();      
+      stage.setScene(new Scene(new HBox(
+         new ImageView(image),
+         new ImageView(finalImage))));
+      stage.show();
+   }
+}
