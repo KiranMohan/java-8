a7cd5c975c03163935340d38aa8a3b2e063661a2
Kiran Mohan <kiranmohan@123.com>
2015-03-10 23:06:16 +0530

    Ch2 Ex1 formatted code

--- java8-for-the-really-impatient/src/main/java/chapter2/Ex1.java
+++ java8-for-the-really-impatient/src/main/java/chapter2/Ex1.java
@@ -18,68 +18,73 @@ import common.Helper;
 
 /**
  * Write a parallel version of the for loop in Section 2.1, “From Iteration to
-Stream Operations,” on page 22. Obtain the number of processors. Make that
-many separate threads, each working on a segment of the list, and total up
-the results as they come in. (You don’t want the threads to update a single
-counter. Why?)
+ * Stream Operations,” on page 22. Obtain the number of processors. Make that
+ * many separate threads, each working on a segment of the list, and total up
+ * the results as they come in. (You don’t want the threads to update a single
+ * counter. Why?)
+ * 
  * @author Kiran Mohan
  *
  */
 public class Ex1 {
-	static final int NUM_OF_PROCESSORS = 4;
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String[] words = Helper.getWordsAsArray("src/main/resources/alice.txt");
-		Predicate<String> filter = w -> w.length() > 6;
-		
-		System.out.println("count(single thead) : " + singleThreadCountWords(words, filter));
-		System.out.println("count(parallelized) : " + parallelCountWords(words, filter));
-	}
+    static final int NUM_OF_PROCESSORS = 4;
 
-	
-	public static int singleThreadCountWords(String [] words, Predicate<String> filter) {
-		int count = 0;
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String[] words = Helper.getWordsAsArray("src/main/resources/alice.txt");
+        Predicate<String> filter = w -> w.length() > 6;
+
+        System.out.println("count(single thead) : "
+                + singleThreadCountWords(words, filter));
+        System.out.println("count(parallelized) : "
+                + parallelCountWords(words, filter));
+    }
+
+    public static int singleThreadCountWords(String[] words,
+            Predicate<String> filter) {
+        int count = 0;
         for (String w : words) {
             if (filter.test(w))
                 count++;
         }
         return count;
-	}
-	
+    }
 
-	/**
-	 * @param words
-	 */
-	public static int parallelCountWords(String[] words, Predicate<String> filter) {
-		
-		ExecutorService executor = Executors.newFixedThreadPool(NUM_OF_PROCESSORS);
-        List<Future<Integer>> results = new ArrayList<Future<Integer>>(NUM_OF_PROCESSORS);
-        
+    /**
+     * @param words
+     */
+    public static int parallelCountWords(String[] words,
+            Predicate<String> filter) {
+
+        ExecutorService executor = Executors
+                .newFixedThreadPool(NUM_OF_PROCESSORS);
+        List<Future<Integer>> results = new ArrayList<Future<Integer>>(
+                NUM_OF_PROCESSORS);
+
         int sliceLength = words.length / NUM_OF_PROCESSORS;
-        for(int from = 0; from < words.length; from += sliceLength) {
-        	int to = from + sliceLength;
-        	String [] slice = Arrays.copyOfRange(words, from, to > words.length ? words.length : to);
-        	results.add(executor.submit(()->singleThreadCountWords(slice, filter)));        	
+        for (int from = 0; from < words.length; from += sliceLength) {
+            int to = from + sliceLength;
+            String[] slice = Arrays.copyOfRange(words, from,
+                    to > words.length ? words.length : to);
+            results.add(executor.submit(
+                    () -> singleThreadCountWords(slice, filter)));
         }
-        
+
         executor.shutdown();
-        
+
         int count = 0;
-        for(Future<Integer> result : results) {
-        	try {
-				count += result.get();
-			} catch (InterruptedException | ExecutionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+        for (Future<Integer> result : results) {
+            try {
+                count += result.get();
+            } catch (InterruptedException | ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
-        
+
         return count;
-	}
-    
-    
+    }
 
 }
